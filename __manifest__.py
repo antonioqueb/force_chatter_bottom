dbd05c1867c8dedbd2ec596ea1639b3bb29b0bee
Antonio Queb <cardstillo@gmail.com>
2026-02-01 00:40:54 -0600

    Actualizar __manifest__.py y refactorizar form_renderer.js para forzar el chatter a la parte inferior, asegurando su correcta visualización en todas las resoluciones.

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,14 @@
-# __manifest__.py
 {
     'name': 'Force Chatter to Bottom',
     'version': '19.0.1.0.0',
     'category': 'Customization/User Interface',
     'summary': 'Mueve siempre el chatter a la parte inferior en todas las vistas.',
-    'description': """
-        Este módulo fuerza al sistema a mostrar siempre el widget de Chatter 
-        (mensajes y notas) en la parte inferior de la vista formulario, 
-        incluso en monitores anchos (pantallas XXL).
-    """,
     'author': 'Alphaqueb Consulting',
-    'depends': ['mail'],
+    'depends': ['web', 'mail'],
     'assets': {
         'web.assets_backend': [
-            'force_chatter_bottom/static/src/css/chatter_position.scss',
+            'force_chatter_bottom/static/src/js/force_chatter_bottom.js',
+            'force_chatter_bottom/static/src/scss/chatter_bottom.scss',
         ],
     },
     'license': 'LGPL-3',
--- static/src/js/form_renderer.js
+++ static/src/js/form_renderer.js
@@ -1,16 +1,21 @@
-// static/src/js/form_renderer.js
 /** @odoo-module **/
 
-import { FormRenderer } from "@web/views/form/form_renderer";
 import { patch } from "@web/core/utils/patch";
+import { FormCompiler } from "@web/views/form/form_compiler";
 
-patch(FormRenderer.prototype, {
-    /**
-     * Override para forzar el chatter siempre abajo
-     * La propiedad isChatterAside determina si el chatter se muestra al lado
-     * Siempre retornamos false para forzarlo abajo
-     */
-    get isChatterAside() {
-        return false;
-    }
+console.log("[force_chatter_bottom] loaded");
+
+patch(FormCompiler.prototype, {
+    compile(node, params) {
+        const res = super.compile(node, params);
+
+        // Ubica el componente OWL del chatter en el árbol compilado
+        const chatter = res.querySelector("t[t-component='__comp__.mailComponents.Chatter']");
+        if (chatter) {
+            // Fuerza el layout a "no-aside"
+            chatter.setAttribute("isChatterAside", "false");
+        }
+
+        return res;
+    },
 });

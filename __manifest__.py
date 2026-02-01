2dea4a1973712074761871731a0c4405f4e10068
Antonio Queb <cardstillo@gmail.com>
2026-01-31 23:17:46 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Force Chatter to Bottom',
+    'version': '19.0.1.0.0',
+    'category': 'Customization/User Interface',
+    'summary': 'Mueve siempre el chatter a la parte inferior en todas las vistas.',
+    'description': """
+        Este módulo fuerza al sistema a mostrar siempre el widget de Chatter 
+        (mensajes y notas) en la parte inferior de la vista formulario, 
+        incluso en monitores anchos (pantallas XXL), evitando que se divida 
+        la pantalla en dos columnas.
+    """,
+    'author': 'Assistant',
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'force_chatter_bottom/static/src/css/chatter_position.css',
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
